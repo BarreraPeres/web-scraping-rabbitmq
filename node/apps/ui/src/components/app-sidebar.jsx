5e590e618659d6ec20fcd83818443d6df759f038
BarreraPeres <gabrielbarreraperes18@gmail.com>
2025-04-12 13:45:15 -0300

    feat: add sidebar / components shadcn

---
+++ node/apps/ui/src/components/app-sidebar.jsx
@@ -0,0 +1,106 @@
+import * as React from "react"
+import {
+    AudioWaveform,
+    BookOpen,
+    Bot,
+    Command,
+    Frame,
+    GalleryVerticalEnd,
+    Map,
+    PieChart,
+    Settings2,
+    SquareTerminal,
+} from "lucide-react"
+
+import { NavMain } from "@/components/nav-main"
+import { NavProjects } from "@/components/nav-projects"
+import { NavUser } from "@/components/nav-user"
+import { TeamSwitcher } from "@/components/team-switcher"
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarHeader,
+    SidebarRail,
+} from "@/components/ui/sidebar"
+
+const data = {
+    teams: [
+        {
+            name: "Acme Inc",
+            logo: GalleryVerticalEnd,
+            plan: "Enterprise",
+        },
+        {
+            name: "Acme Corp.",
+            logo: AudioWaveform,
+            plan: "Startup",
+        },
+        {
+            name: "Evil Corp.",
+            logo: Command,
+            plan: "Free",
+        },
+    ],
+    navMain: [
+        {
+            title: "Playground",
+            url: "#",
+            icon: SquareTerminal,
+            isActive: true,
+            items: [
+                {
+                    title: "Inicial",
+                    url: "#",
+                },
+                {
+                    title: "Em Observação",
+                    url: "#",
+                },
+                {
+                    title: "Configuração",
+                    url: "#",
+                },
+            ],
+        },
+        {
+            title: "Produtos",
+            url: "#",
+            icon: Bot,
+            items: [
+                {
+                    title: "Adicionar",
+                    url: "#",
+                },
+                {
+                    title: "Explorar",
+                    url: "#",
+                },
+                {
+                    title: "Histórico",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+
+}
+
+export function AppSidebar({
+    ...props
+}) {
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <TeamSwitcher teams={data.teams} />
+            </SidebarHeader>
+            <SidebarContent>
+                <NavMain items={data.navMain} />
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser />
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    );
+}
